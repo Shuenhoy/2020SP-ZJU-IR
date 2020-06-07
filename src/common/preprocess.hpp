9c3004067c76baec6fc68742e0142e5df1f69b81
Guo <msn4399@163.com>
2020-06-07 16:57:20 +0800

    implement ir::common::tokenize

--- src/common/preprocess.hpp
+++ src/common/preprocess.hpp
@@ -1,13 +1,27 @@
 #pragma once
 
 #include "common.hpp"
+#include <cctype>
 #include <string>
 #include <string_view>
 #include <vector>
 
 namespace ir::common {
+
+/* 将字符串变成 token，目前仅仅保留了字母与数字，其他符号全部去除 */
 inline std::vector<std::string_view> tokenize(std::string_view input) {
-    NOT_IMPLEMENTED;
+    std::vector<std::string_view> tokens;
+    for (std::size_t i = 0; i < input.size(); i++) {
+        if (std::isalnum(input[i])) {
+            std::size_t begin = i;
+            while (std::isalnum(input[++i]))
+                ;
+            std::size_t length = i - begin;
+            std::string_view token(input.data() + begin, length);
+            tokens.push_back(token);
+        }
+    }
+    return tokens;
 }
 
 inline std::string unify_token(std::string_view input) {
