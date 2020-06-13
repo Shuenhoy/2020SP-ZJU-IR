25c7224a5b10b791446f744c3d421fdd989ddb67
HaoranSUN <shuenhoy@zju.edu.cn>
2020-06-13 07:58:04 +0000

    add vec display

--- src/common/debug.hpp
+++ src/common/debug.hpp
@@ -5,16 +5,27 @@
 #include "index.hpp"
 #include "kgram.hpp"
 #include "lead_follow_inv_index.hpp"
+#include "vec.hpp"
 
 #include <fstream>
 
 namespace ir::common::debug {
 
-inline void display(const Dictionary::Element &token, const Dictionary &dict, std::ofstream &fout) {
+inline void display(const vec::Vec &vec, const Dictionary &dict, const DocInvIndex &index, std::ostream &fout) {
+    fout << "(";
+    for (auto &&[token, value] : vec) {
+        fout << "{";
+        fout << dict.get(index.items[token]) << ": " << value;
+        fout << "}, ";
+    }
+    fout << ")";
+}
+
+inline void display(const Dictionary::Element &token, const Dictionary &dict, std::ostream &fout) {
     fout << dict.get(token) << "(" << token.pos << "," << token.len << ")";
 }
 
-inline void display(const DocInvIndexElement &ele, std::ofstream &fout) {
+inline void display(const DocInvIndexElement &ele, std::ostream &fout) {
     fout << "{ ID = " << ele.doc_id;
     for (auto x : ele.positions) {
         fout << ", " << x;
@@ -24,7 +35,7 @@ inline void display(const DocInvIndexElement &ele, std::ofstream &fout) {
 
 template <typename T>
 inline void display(const Dictionary &dict,
-                    const Index<Dictionary::Element, T> &index, std::ofstream &fout) {
+                    const Index<Dictionary::Element, T> &index, std::ostream &fout) {
     fout << "[Dictionary Begin]\n";
     fout << "string length: " << dict.dic.size() << "\n";
     fout << "tokens: " << index.items.size() << "\n";
@@ -36,7 +47,7 @@ inline void display(const Dictionary &dict,
     fout.flush();
 }
 
-inline void display(DocInvIndex &index, const Dictionary &dict, std::ofstream &fout) {
+inline void display(DocInvIndex &index, const Dictionary &dict, std::ostream &fout) {
     fout << "[DocIndex Begin]\n";
     for (auto x : index.items) {
         display(x, dict, fout);
@@ -52,7 +63,7 @@ inline void display(DocInvIndex &index, const Dictionary &dict, std::ofstream &f
     fout.flush();
 }
 
-inline void display(KGramInvIndex &index, const Dictionary &kdict, const Dictionary &dict, std::ofstream &fout) {
+inline void display(KGramInvIndex &index, const Dictionary &kdict, const Dictionary &dict, std::ostream &fout) {
     fout << "[KGramIndex Begin]\n";
     for (auto x : index.items) {
         display(x, kdict, fout);
@@ -68,7 +79,7 @@ inline void display(KGramInvIndex &index, const Dictionary &kdict, const Diction
     fout.flush();
 }
 
-inline void display(const LeadFollowInvIndex &lead_follow, std::ofstream &fout) {
+inline void display(const LeadFollowInvIndex &lead_follow, std::ostream &fout) {
     fout << "[LeadFollow Begin]\n";
     for (auto x : lead_follow.items) {
         fout << x << ": [";
@@ -81,7 +92,7 @@ inline void display(const LeadFollowInvIndex &lead_follow, std::ofstream &fout) 
     fout.flush();
 }
 
-inline void display(DocumentInfos &docinfos, std::ofstream &fout) {
+inline void display(DocumentInfos &docinfos, std::ostream &fout) {
     fout << "[DocInfos Begin]\n";
 
     for (auto &&x : docinfos) {
