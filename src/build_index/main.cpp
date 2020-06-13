af123d705ecfdfb748e84176f77fe7c232b02df8
HaoranSUN <shuenhoy@zju.edu.cn>
2020-06-13 11:47:54 +0000

    add time measture

--- src/build_index/main.cpp
+++ src/build_index/main.cpp
@@ -14,27 +14,52 @@
 using namespace ir;
 using namespace std::string_literals;
 
+#define MEASURE_BGN(x, y)                     \
+    do {                                      \
+        x = std::chrono::steady_clock::now(); \
+    } while (false)
+
+#define MEASURE_END(x, y)                                                                                     \
+    do {                                                                                                      \
+        y = std::chrono::steady_clock::now();                                                                 \
+        double duration = (std::chrono::duration_cast<std::chrono::microseconds>(y - x).count()) / 1000000.0; \
+        std::cout << "done in " << duration << "s" << std::endl;                                              \
+    } while (false)
+
 int main(int argc, char *argv[]) {
     if (argc != 4) {
         std::cerr << "usage: build_index <input dir> <output dir> <kgram>" << std::endl;
         return -1;
     }
+    std::chrono::_V2::steady_clock::time_point begin_time, end_time;
 
     std::string output_dir = argv[2];
 
     size_t k = std::atol(argv[3]);
+
+    MEASURE_BGN(begin_time, end_time);
     std::cout << "building inv doc index" << std::endl;
     auto [raw, N] = build_index::build_raw(std::filesystem::directory_iterator(argv[1]));
     auto [doc_index, dict] = build_index::raw_to_fi(raw);
+    MEASURE_END(begin_time, end_time);
+
+    MEASURE_BGN(begin_time, end_time);
     std::cout << "building doc infos" << std::endl;
-
     auto doc_infos = build_index::build_doc_infos(std::filesystem::directory_iterator(argv[1]), dict, doc_index, N);
+    MEASURE_END(begin_time, end_time);
+
+    MEASURE_BGN(begin_time, end_time);
+    std::cout << "building kgram index" << std::endl;
     auto [kgram, kgram_dict] = build_index::raw_to_fi(build_index::build_kgram(dict, doc_index, k));
+    MEASURE_END(begin_time, end_time);
+
+    MEASURE_BGN(begin_time, end_time);
     std::cout << "building lead follow index" << std::endl;
-
     auto lead_follow = build_index::build_lead_follow(doc_infos, dict, doc_index);
-    std::cout << "writing to disk" << std::endl;
+    MEASURE_END(begin_time, end_time);
 
+    MEASURE_BGN(begin_time, end_time);
+    std::cout << "writing to disk" << std::endl;
     {
         std::ofstream fout(output_dir + "/doc.index", std::ios_base::binary);
         doc_index.serialize(fout);
@@ -59,8 +84,12 @@ int main(int argc, char *argv[]) {
         std::ofstream fout(output_dir + "/doc.infos", std::ios_base::binary);
         common::Serialization<common::DocumentInfos>::serialize(fout, doc_infos);
     }
-    std::cout << "done." << std::endl;
+    MEASURE_END(begin_time, end_time);
+
 #ifndef NDEBUG
+    MEASURE_BGN(begin_time, end_time);
+    std::cout << "writing debug logs" << std::endl;
+
     std::ofstream fout(output_dir + "/debug.log");
 
     common::debug::display(doc_infos, fout);
@@ -70,7 +99,10 @@ int main(int argc, char *argv[]) {
 
     common::debug::display(kgram, kgram_dict, dict, fout);
     common::debug::display(doc_index, dict, fout);
+    MEASURE_END(begin_time, end_time);
+
 #endif
+    std::cout << "done." << std::endl;
 
     return 0;
 }
