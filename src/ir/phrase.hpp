410f543d8d2d506e48ad463d7b5343d9bf06cbc1
Guo Shuai <msn4399@163.com>
2020-06-16 12:23:50 +0800

    fix phrase_merge

--- src/ir/phrase.hpp
+++ src/ir/phrase.hpp
@@ -5,29 +5,32 @@
 #include <vector>
 
 namespace ir::ir {
-inline std::vector<common::DocInvIndexElement> phrase_merge(const std::vector<common::DocInvIndexElement> &a,
-                                                            const std::vector<common::DocInvIndexElement> &b) {
+inline std::vector<common::DocInvIndexElement>
+phrase_merge(const std::vector<common::DocInvIndexElement> &a,
+             const std::vector<common::DocInvIndexElement> &b) {
     std::vector<common::DocInvIndexElement> ret;
-    for (size_t i = 0; i < a.size(); i++) {
-        for (size_t j = 0; j < b.size(); j++) {
-            if (a[i].doc_id < b[j].doc_id)
-                i++;
-            else if (a[i].doc_id > b[j].doc_id)
-                j++;
-            else {
-                common::DocInvIndexElement ele;
-                ele.doc_id = a[i].doc_id;
-                for (size_t pos_b : b[j].positions) {
-                    for (size_t k = 0; a[i].positions[k] < pos_b; k++) {
-                        if (a[i].positions[k] == pos_b - 1) {
-                            ele.positions.push_back(pos_b);
-                        }
+    size_t i = 0, j = 0;
+    while (i < a.size() && j < b.size()) {
+        if (a[i].doc_id < b[j].doc_id)
+            i++;
+        else if (a[i].doc_id > b[j].doc_id)
+            j++;
+        else {
+            common::DocInvIndexElement ele;
+            ele.doc_id = a[i].doc_id;
+            for (size_t pos_b : b[j].positions) {
+                for (size_t k = 0; a[i].positions[k] < pos_b; k++) {
+                    if (a[i].positions[k] == pos_b - 1) {
+                        ele.positions.push_back(pos_b);
                     }
                 }
-
-                if (ele.positions.size() > 0)
-                    ret.push_back(ele);
             }
+
+            if (ele.positions.size() > 0)
+                ret.push_back(ele);
+            
+            i++;
+            j++;
         }
     }
     return ret;
